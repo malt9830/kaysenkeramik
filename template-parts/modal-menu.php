05f9c8a6eb814c9ac28647a1f9cbfb6ae9eee911
malt9830 <70881301+malt9830@users.noreply.github.com>
2021-06-05 15:27:37 +0200

    title burger menu

--- template-parts/modal-menu.php
+++ template-parts/modal-menu.php
@@ -16,8 +16,11 @@
         <div class="menu-wrapper section-inner">
 
             <div class="menu-top">
-                <button class="toggle close-nav-toggle fill-children-current-color" data-toggle-target=".menu-modal" data-toggle-body-class="showing-menu-modal" aria-expanded="false" data-set-focus=".menu-modal">
-                    <img src="https://malthekusk.one/kea/kaysenkeramik/wordpress/wp-content/uploads/2021/05/Group-91.svg" alt="svg cross for hiding burger menu"> </button><!-- .nav-toggle -->
+                <div class="site-title faux-heading">
+                    <a href="https://malthekusk.one/kea/kaysenkeramik/wordpress/">Kaysen Keramik</a>
+                    <button class="toggle close-nav-toggle fill-children-current-color" data-toggle-target=".menu-modal" data-toggle-body-class="showing-menu-modal" aria-expanded="false" data-set-focus=".menu-modal">
+                        <img src="https://malthekusk.one/kea/kaysenkeramik/wordpress/wp-content/uploads/2021/05/Group-91.svg" alt="svg cross for hiding burger menu"> </button><!-- .nav-toggle -->
+                </div>
                 <div class="header-underline"></div>
                 <?php
 
